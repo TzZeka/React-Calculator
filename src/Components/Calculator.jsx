193b00518bc527c6b4b0d1e227b65c2b12dec1d2
Tsvetomir <1hundredheads@gmail.com>
2025-02-21 16:42:00 +0000

    modified

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,89 +1,122 @@
-import React, { useState } from 'react'
-import './calculator.css';
+import React, { useState } from "react";
+import "./calculator.css";
 const Calculator = () => {
   let [result, setResult] = useState("");
 
   const handleClick = (e) => {
-    if(result.length >=16){
+    if (result.length >= 16) {
       setResult("!Tooo Big Input");
       return;
     }
-    if (result.charAt(0) === '0') {
-      result = result.slice(1, result.length)
+    if (result.charAt(0) === "0") {
+      result = result.slice(1, result.length);
     }
     setResult(result.concat(e.target.name));
-  }
+  };
 
   const clear = () => {
     setResult("");
-  }
+  };
   const backSpace = () => {
-    setResult(result.slice(0, result.length - 1))
-  }
+    setResult(result.slice(0, result.length - 1));
+  };
 
   const calculate = () => {
     try {
-      result=eval(result).toString()
-      if(result.includes('.')){
-        result=+ eval(result);
-        result=result.toFixed(4).toString();
+      result = eval(result).toString();
+      if (result.includes(".")) {
+        result = +eval(result);
+        result = result.toFixed(4).toString();
         setResult(result);
-      }else{
+      } else {
         setResult(eval(result).toString());
       }
-      
     } catch (err) {
       setResult("Error");
     }
-
-  }
+  };
 
   return (
-    <div className='container'>
-      <h3>Calculator</h3>
+    <div className="container">
+      <h3>Simple React Calculator</h3>
       <form action="">
         <input type="text" value={result} />
       </form>
 
-      <div className='calculator'>
+      <div className="calculator">
         <section action="">
-            <div className='special'>
-                <button onClick={clear} className="clear color">CE</button>
-                <button name="/" className='color' onClick={handleClick}>/</button>
-                <button name="*" className='color' onClick={handleClick}>*</button>
-                <button onClick={backSpace} className="backspace color">C</button>
-            </div>
-        
-            <div>
-                <button name="7" onClick={handleClick}>7</button>
-                <button name="8" onClick={handleClick}>8</button>
-                <button name="9" onClick={handleClick}>9</button>
-                <button name="-" className='color' onClick={handleClick}>-</button>
-                
-            </div>
-            <div>
-                <button name="4" onClick={handleClick}>4</button>
-                <button name="5" onClick={handleClick}>5</button>
-                <button name="6" onClick={handleClick}>6</button>
-                <button name="+" className='color' onClick={handleClick}>+</button>
-                
-            </div>
-            <div>
-                <button name="1" onClick={handleClick}>1</button>
-                <button name="2" onClick={handleClick}>2</button>
-                <button name="3" onClick={handleClick}>3</button>
-                
-            </div>
-            <div>
-                <button name="00" onClick={handleClick}>00</button>
-                <button name="0" onClick={handleClick}>0</button>
-                <button name="," className='color' onClick={handleClick}>,</button>
-                <button onClick={calculate} className="equal color">=</button>
-            </div>
-        </section>
-      </div>    
-      </div>
-  )
-}
+          <div className="special">
+            <button onClick={clear} className="clear color">
+              CE
+            </button>
+            <button name="/" className="color" onClick={handleClick}>
+              /
+            </button>
+            <button name="*" className="color" onClick={handleClick}>
+              *
+            </button>
+            <button onClick={backSpace} className="backspace color">
+              C
+            </button>
+          </div>
 
-export default Calculator
+          <div>
+            <button name="7" onClick={handleClick}>
+              7
+            </button>
+            <button name="8" onClick={handleClick}>
+              8
+            </button>
+            <button name="9" onClick={handleClick}>
+              9
+            </button>
+            <button name="-" className="color" onClick={handleClick}>
+              -
+            </button>
+          </div>
+          <div>
+            <button name="4" onClick={handleClick}>
+              4
+            </button>
+            <button name="5" onClick={handleClick}>
+              5
+            </button>
+            <button name="6" onClick={handleClick}>
+              6
+            </button>
+            <button name="+" className="color" onClick={handleClick}>
+              +
+            </button>
+          </div>
+          <div>
+            <button name="1" onClick={handleClick}>
+              1
+            </button>
+            <button name="2" onClick={handleClick}>
+              2
+            </button>
+            <button name="3" onClick={handleClick}>
+              3
+            </button>
+          </div>
+          <div>
+            <button name="00" onClick={handleClick}>
+              00
+            </button>
+            <button name="0" onClick={handleClick}>
+              0
+            </button>
+            <button name="." className="color" onClick={handleClick}>
+              .
+            </button>
+            <button onClick={calculate} className="equal color">
+              =
+            </button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
